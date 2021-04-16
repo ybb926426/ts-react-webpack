0457eb4ad79c837eb788b789183ba95dd879e823
yangbo568969859 <568969859@qq.com>
2021-04-16 09:14:33 +0800

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
   let myName = 'Tom';
   console.log(`My name is ${myNane}`);
   console.log(`My name is ${myName.toStrng()}`);
+  console.log(`My name is ${myName.toStrng()}`);
   console.log(`My name is ${myName}`)
   return (
     <div className="App">
